2a7e864c3edbe89487e1521d67f2ea3e4b0fd6bf
= <=>
2020-10-10 21:00:06 -0400

    made invoice notification email longer so itll be less likely to be caught in spam filters

--- auctions/management/commands/email_invoice.py
+++ auctions/management/commands/email_invoice.py
@@ -7,14 +7,23 @@ from django.core.mail import send_mail
 def notify(email, auction, pk):
     subject = "Your auction invoice"
     if auction:
-        subject = auction
+        subject = f"Your auction invoice for {auction}"
+    msg = f'Thanks for bidding!  You can view your invoice here: https://auctions.toxotes.org/invoices/{pk}/\n\n'
+    if auction:
+        thisAuction = Auction.objects.get(pk=auction.pk)
+        creator = User.objects.get(pk=thisAuction.created_by.pk)
+        contactEmail = creator.email
+        msg += f"You must meet at {thisAuction.pickup_location}"
+        if thisAuction.alternate_pickup_location:
+            msg += f" or {thisAuction.alternate_pickup_location}"
+        msg += f" to pay and exchange your lots.\n\nSee you there!\n\nIf you have questions, please contact {contactEmail}\n\n"
+    msg += "Please don't reply to this email.\n\nBest,\nauctions.toxotes.org"
     send_mail(
         subject,
-        f'Thanks for bidding!  You can view your invoice here: https://auctions.toxotes.org/invoices/{pk}/',
+        msg,
         'TFCB notifications',
         [email],
         fail_silently=False,
-        html_message = f'Thanks for bidding!  <a href="https://auctions.toxotes.org/invoices/{pk}/">View your invoice here</a>'
     )
 
 class Command(BaseCommand):
