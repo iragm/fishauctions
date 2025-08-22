be3d60d8cbf78fd4477afbebbd9fc61d9ac7cfae
= <=>
2025-08-22 01:21:54 +0000

    comfirming that it's not the log folder that's causing issues.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 if [ "$(id -u)" = "0" ]; then
   echo "Fixing ownership on media/static..."
   chown -R app:app /home/app/web/mediafiles /home/app/web/staticfiles || true
-  chown -R app:app /home/app/web/logs || true
+  #chown -R app:app /home/app/web/logs || true
 
   # Drop privileges and re-exec this script as app
   exec su app -c "/entrypoint.sh $*"
