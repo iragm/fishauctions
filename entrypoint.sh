f385d05b7dbeac6299e1ccd9c8d7a1cfb458a49f
= <=>
2025-08-22 15:43:13 +0000

    remove chown altogether

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 # If we're root and not in CI, fix permissions on docker bind mounts, then re-run this script as app
 if [ "$(id -u)" = "0" ] && [ -z "$CI" ]; then
   echo "Fixing ownership on media/static..."
-  chown -R app:app /home/app/web/mediafiles /home/app/web/staticfiles || true
-  chown -R app:app /home/app/web/logs || true
+  #chown -R app:app /home/app/web/mediafiles /home/app/web/staticfiles || true
+  #chown -R app:app /home/app/web/logs || true
 
   # Drop privileges and re-exec this script as app
   exec su app -c "/entrypoint.sh $*"
