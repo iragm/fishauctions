536fdeb7f7c6d80d17b9f645d97271ea2dc109f7
Ira <71519690+iragm@users.noreply.github.com>
2025-11-11 08:53:18 -0500

    Update prepare-ci.sh so that tests run in a dev env
    .

--- .github/scripts/prepare-ci.sh
+++ .github/scripts/prepare-ci.sh
@@ -2,5 +2,10 @@
 
 cp .env.example .env
 
+sed -i "/^DEBUG='False'/d" .env
+sed -i "/^NGINX_IMAGE='lscr.io\/linuxserver\/swag'/d" .env
+sed -i "/^NGINX_CONF='.\/nginx.prod.conf'/d" .env
+sed -i "/^NGINX_CONF_LOCATION='\/config\/nginx\/site-confs\/default.conf'/d" .env
+
 mkdir logs
 chmod -R 777 logs
