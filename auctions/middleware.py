5a5134ba7515e442e3bc7b73463d65756336091d
= <=>
2025-11-19 16:20:46 +0000

    add js and css resources to static

--- auctions/middleware.py
+++ auctions/middleware.py
@@ -6,10 +6,10 @@ Custom middleware for the auctions application.
 class CrossOriginIsolationMiddleware:
     """
     Middleware to add Cross-Origin-Opener-Policy and Cross-Origin-Embedder-Policy headers.
-    
+
     These headers are required for SharedArrayBuffer and WebAssembly.Memory serialization,
     which are needed by the Vosklet voice recognition library.
-    
+
     More info: https://web.dev/coop-coep/
     """
 
@@ -18,12 +18,12 @@ class CrossOriginIsolationMiddleware:
 
     def __call__(self, request):
         response = self.get_response(request)
-        
+
         # Add COOP header - allows popups while isolating the browsing context
         # Using 'same-origin' would be more secure but breaks OAuth popups
         response["Cross-Origin-Opener-Policy"] = "same-origin-allow-popups"
-        
+
         # Add COEP header - requires all resources to be loaded with CORS or same-origin
         response["Cross-Origin-Embedder-Policy"] = "require-corp"
-        
+
         return response
